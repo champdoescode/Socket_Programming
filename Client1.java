ffaa60a18a1243e1f73e5120bef4145f21c6e7b9
champdoescode <85624223+champdoescode@users.noreply.github.com>
2022-07-27 10:11:03 +0530

    Add files via upload
    Initial Commit

---
+++ Client1.java
@@ -0,0 +1,34 @@
+package socket_programming;
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+public class Client1 {
+		
+	public static void main(String args[]) throws Exception
+	{
+		Socket sk= new Socket("127.0.0.1",5000);
+		BufferedReader sin = new BufferedReader(new InputStreamReader(sk.getInputStream()));
+		PrintStream sout = new PrintStream(sk.getOutputStream());
+		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		while(true)
+		{
+			System.out.println("Client : ");
+			s=stdin.readLine();
+			sout.println(s);
+			if(s.equalsIgnoreCase("BYE"))
+			{
+				System.out.println("Connection is Ended from Client Side");
+				break;
+			}
+			s=sin.readLine();
+			System.out.print("Server : "+s+"\n");
+		}
+		sk.close();
+		sin.close();
+		sout.close();
+		stdin.close();
+	}
+
+}
